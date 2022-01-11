8cccd5f3c344a3bf2900ded8c82469e5b47a9a3c
allenhyfay <609357776@qq.com>
2022-01-11 18:16:17 +0800

    com

--- mustache/templates/chainlink/contracts.js
+++ mustache/templates/chainlink/contracts.js
@@ -1,9 +1,9 @@
-vmodule.exports = [
+module.exports = [
   {
     prod: 10557958,
     test: null,
     type: 'chainlink',
-    name: 'CAKE/BNB',
+    name: 'CAKEBNB',
     address: "'0xcb23da9ea243f53194cbc2380a6d4d9bc046161f'",
   }
 ];
--- mustache/templates/chainlink/create-yaml.js
+++ mustache/templates/chainlink/create-yaml.js
@@ -1,5 +1,5 @@
-const Contracts = require('../chainlink/contracts');
-const ChainlinkSpecificContracts = require('./contracts');
+
+const Contracts = require('./contracts');
 const { createStartBlock } = require('../common');
 
 // TODO - If Synthetix ran any aggregators that were not provided by
@@ -13,36 +13,29 @@ module.exports = {
       mappingFile: '../src/chainlink-mapping.ts',
       startBlock: createStartBlock(startBlocks, env, universalTestBlock, false),
       address,
-      abi: 'Aggregator',
-      entities: ['AggregatorAnswer'],
+      abi: 'Contract',
+      entities: ['ResponseReceived','OwnershipRenounced','OwnershipTransferred','ChainlinkRequested','ChainlinkFulfilled' ,
+      'ChainlinkCancelled','AnswerUpdated','NewRound'],
       abis: [
         {
           name: 'Contract',
           path: '../abis/Contract.json',
         }
       ],
-     /* events: [
+      events: [
         {
           event: 'AnswerUpdated(indexed int256,indexed uint256,uint256)',
           handler: 'handleAggregatorAnswerUpdated',
         },
-      ],*/
-      blockHandlers:[
-        {
-          handler:'handleBlock'
-        }
       ]
     });
 
     // note that the filter here will have to be changed and this section updated
     // as chainlink adds new contract types
-    return [...Contracts, ...ChainlinkSpecificContracts]
-      .filter(({ type }) => type === 'aggregator')
-      .map(({ name, prod, test, address }) => {
-        if (!SYNTHETIX_AGGREGATOR_NAMES.includes(name)) {
-          const startBlocks = { prod, test };
-          return createAggregatorBlock({ name, startBlocks, address });
-        }
-      });
+
+    return Contracts.map(({ prod, test, type, name, address }) => {
+      const startBlocks = { prod, test, exchanger: null };
+        return createAggregatorBlock({ startBlocks, name, address });
+    });
   },
 };
