b7d33993a9087ef9732d71dec3c7444dc5dd64e5
allen <allen@0block.com>
2022-01-11 17:45:52 +0800

    tiaojiao
    Signed-off-by: allen <allen@0block.com>

--- mustache/templates/chainlink/create-yaml.js
+++ mustache/templates/chainlink/create-yaml.js
@@ -1,4 +1,4 @@
-const Contracts = require('../rates/contracts');
+const Contracts = require('../chainlink/contracts');
 const ChainlinkSpecificContracts = require('./contracts');
 const { createStartBlock } = require('../common');
 
@@ -17,24 +17,21 @@ module.exports = {
       entities: ['AggregatorAnswer'],
       abis: [
         {
-          name: 'Aggregator',
-          path: '../abis/Aggregator.json',
-        },
-        {
-          name: 'ExchangeRates',
-          path: '../abis/ExchangeRates.json',
-        },
-        {
-          name: 'AddressResolver',
-          path: '../abis/AddressResolver.json',
-        },
+          name: 'Contract',
+          path: '../abis/Contract.json',
+        }
       ],
-      events: [
+     /* events: [
         {
           event: 'AnswerUpdated(indexed int256,indexed uint256,uint256)',
           handler: 'handleAggregatorAnswerUpdated',
         },
-      ],
+      ],*/
+      blockHandlers:[
+        {
+          handler:'handleBlock'
+        }
+      ]
     });
 
     // note that the filter here will have to be changed and this section updated
